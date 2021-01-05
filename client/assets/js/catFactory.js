34974d83df1f35f01d6e2c6ba3c472c9ed41cad4
anditech <71329092+anditech@users.noreply.github.com>
2021-01-04 22:49:51 -0300

    HTML and JavaScript Update
    Updated form group on HTML:
    Dress & Bracelet Shape.
    created new id to merge dress and bracelet functionalities
    
    Updated DNA span dragttributes
    
    Updated JS - catSettings
    defaultDNA
    function renderCat
    Created  new function to merge functionalities dressShape and braceletShape
    
    Updated JS - catFactory
    Created new function fashionVariation to merge shape functionalities

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -16,27 +16,33 @@ function genColors(){
 
 //This function code needs to modified so that it works with Your cat code.
 function headColor(color,code) {
-    $('.head, .arms').css('background', '#' + color)  //This changes the color of the cat
-    $('#headcode').html('code: ' + code) //This updates text of the badge next to the slider
-    $('#dnabody').html(code) //This updates the body color part of the DNA that is displayed below the cat
+    $('.head, .arms').css('background', '#' + color);  //This changes the color of the cat
+    $('#headcode').html('code: ' + code); //This updates text of the badge next to the slider
+    $('#dnabody').html(code); //This updates the body color part of the DNA that is displayed below the cat
+}
+
+function mouthColor(color,code) {
+    $('.mouth').css('background', '#' + color);  //This changes the color of the mouth
+    $('#mouthCode').html('code: ' + code);  //This updates text of the badge next to the slider
+    $('#dnamouth').html(code);  //This updates the mouth color part of the DNA that is displayed below the cat
 }
 
 function eyeColor(color,code) {
-    $('.pupils').css('background', '#' + color)  // This changes thre color of the eyes
-    $('#eyeCode').html('code: ' + code)  // This updates text of the badge next to slider
-    $('#dnaeyes').html(code)  //This updates the body coler part of the DNA that is displayed below the cat 
+    $('.pupils').css('background', '#' + color);  // This changes thre color of the eyes
+    $('#eyeCode').html('code: ' + code);  // This updates text of the badge next to slider
+    $('#dnaeyes').html(code);  //This updates the body color part of the DNA that is displayed below the cat 
 }
 
 function flowerColor(color,code) {
-    $('.flower').css('background', '#' + color)
-    $('#flowerCode').html('code: ' + code)
-    $('#dnaflower').html(code)
+    $('.flower').css('background', '#' + color);
+    $('#flowerCode').html('code: ' + code);
+    $('#dnaflower').html(code);
 }
 
 function dressColor(color, code) {
-    $('.dress').css('background', '#' + color)
-    $('#dressCode').html('code: ' + code)
-    $('#dnadress').html(code)
+    $('.dress').css('background', '#' + color);
+    $('#dressCode').html('code: ' + code);
+    $('#dnadress').html(code);
 }
 
 
@@ -71,48 +77,104 @@ function eyeVariation(num) {
             break; 
     }
 }
-// Bracelet Vairations
-function decorationVariation(num) {
+// Dress & Bracelet Vairations
+function fashionVariation(num) {
 
-    $('#dnaBraceletShape').html(num)
+    $('#dnaFashionShape').html(num)
     switch (num) {
         case 1:
+            normalDress();
             normalDecoration();  
-            $('#decorationName').html('Wide'); // Set badge to Wide          
+            $('#fashionName').html('Free | Wide'); // Set badge to Free Wide          
             break; 
         case 2:
+            normalDress();
             normalDecoration();  // Reset
-            $('#decorationName').html('Medium'); // Set badge to Fine            
+            $('#fashionName').html('Neck | Medium'); // Set badge to Neck Medium     
+            dressType1();     
             decorationType1();
             break;   
         case 3:
+            normalDress();
             normalDecoration();  // Reset
-            $('#decorationName').html('Fine'); // Set badge to FineYellow             
+            $('#fashionName').html('Shoulder | Fine'); // Set badge to Shoulder Fine      
+            dressType2();       
             decorationType2();
             break; 
+        case 3:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Shoulder | Fine'); // Set badge to Shoulder Fine      
+            dressType2();       
+            decorationType2();
+            break;
+        case 4:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Free | Medium'); // Set badge to Free Medium      
+            normalDress();      
+            decorationType1();
+            break
+        case 5:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Neck | Fine'); // Set badge to Neck Fine      
+            dressType1();        
+            decorationType2();
+            break;   
+        case 6:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Shoulder | Wide'); // Set badge to Shoulder Wide     
+            dressType2();       
+            normalDecoration(); 
+            break;
+        case 7:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Free | Fine'); // Set badge to Free Fine      
+            normalDress();        
+            decorationType2();
+            break; 
+        case 8:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Neck| Wide'); // Set badge to Neck Wide      
+            dressType1();        
+            normalDecoration();
+            break;
+        case 9:
+            normalDress();
+            normalDecoration();  // Reset
+            $('#fashionName').html('Shoulder | Medium'); // Set badge to Shoulder Medium      
+            dressType2();        
+            decorationType1();
+            break;    
     }
 }
-// Dress Variations
+/*
 function dressVariation(num) {
 
     $('#dnaDressShape').html(num)
     switch (num) {
         case 1: 
             normalDress();
-            $('#dressName').html('Free'); // Set badge to round
+            $('#dressName').html('Free'); // Set badge to Free
             break;
         case 2:
             normalDress();  // Reset
-            $('#dressName').html('Neck'); // Set badge to shoulders
+            $('#dressName').html('Neck'); // Set badge to Neck
             dressType1();
             break;
         case 3:
             normalDress();  // Reset
-            $('#dressName').html('Shoulder'); // Set badge to shoulders
+            $('#dressName').html('Shoulder'); // Set badge to Shoulders
             dressType2();
             break;   
     }
 }
+*/
+
 // Animation
 function animationVariation(num) {
 
--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -3,43 +3,32 @@ var colors = Object.values(allColors())
 
 var defaultDNA = {
     "headcolor" : 19,
-    "dressColor" : 70,
+    "mouthColor": 83,
     "eyesColor" : 79,
     "flowerColor" : 10,
+    "dressColor" : 70,
+    
     //Cattributes
     "eyeShape" : 1,
-    "braceletShape" : 1,
-    "dressShape" : 1,
+    "fashionShape" : 3,
+  
     // Animation
-    "animation" :  1,
-
-    /*
-    "decorationMidcolor" : 13,
-    "decorationSidescolor" : 13,
-    
-    "lastNum" :  1
-    */
+    "animation" : 2,
+    "lastNum" :  1,
     }
 
 // when page load
 $( document ).ready(function() {
   $('#dnabody').html(defaultDNA.headcolor);
-  $('#dnadress').html(defaultDNA.dressColor);
-  $('#dnaflower').html(defaultDNA.flowerColor);
+  $('#dnamouth').html(defaultDNA.mouthColor);
   $('#dnaeyes').html(defaultDNA.eyesColor);
-    
+  $('#dnaflower').html(defaultDNA.flowerColor);
+  $('#dnadress').html(defaultDNA.dressColor);  
   $('#dnaEyeShape').html(defaultDNA.eyeShape);
-  $('#dnaBraceletShape').html(defaultDNA.braceletShape);
-  $('#dnaDressShape').html(defaultDNA.dressShape);
+  $('#dnaFashionShape').html(defaultDNA.fashionShape);
   $('#dnaanimation').html(defaultDNA.animation);
-  
-
-
-  /*
-  $('#dnadecorationSides').html(defaultDNA.decorationSidescolor)
-  
   $('#dnaspecial').html(defaultDNA.lastNum)
-  */
+  
 
   renderCat(defaultDNA)
 });
@@ -47,25 +36,23 @@ $( document ).ready(function() {
 function getDna(){
     var dna = ''
     dna += $('#dnabody').html()
-    dna += $('#dnadress').html()
+    dna += $('#dnamouth').html()
     dna += $('#dnaeyes').html()
     dna += $('#dnaflower').html()
-
+    dna += $('#dnadress').html()
     dna += $('#dnaEyeShape').html()
-    dna += $('#dnaBraceletShape').html()
-    dna += $('#dnaDressShape').html()
+    dna += $('#dnaFashionShape').html()
     dna += $('#dnaanimation').html()
-    /*
-    dna += $('#dnadecorationSides').html()
-    
     dna += $('#dnaspecial').html()
-    */
+    
     return parseInt(dna)
 }
 
 function renderCat(dna){
     headColor(colors[dna.headcolor],dna.headcolor)
     $('#bodycolor').val(dna.headcolor)
+    mouthColor(colors[dna.mouthColor],dna.mouthColor)
+    $('#mouthColor').val(dna.mouthColor)
     eyeColor(colors[dna.eyesColor],dna.eyesColor)
     $('#eyeColor').val(dna.eyesColor)
     flowerColor(colors[dna.flowerColor],dna.flowerColor)
@@ -74,12 +61,10 @@ function renderCat(dna){
     $('#dressColor').val(dna.dressColor)
     eyeVariation(dna.eyeShape)
     $('#eyeShape').val(dna.eyeShape)
-    decorationVariation(dna.braceletShape)
-    $('#braceletShape').val(dna.braceletShape)
-    dressVariation(dna.dressShape)
-    $('#dressShape').val(dna.dressShape)
+    fashionVariation(dna.fashionShape)
+    $('#fashionShape').val(dna.fashionShape)
     animationVariation(dna.animation)
-    $("#animation").val(dna.animation)
+    $('#animation').val(dna.animation)
 
 }
 // Changing cat colors
@@ -88,39 +73,36 @@ $('#bodycolor').change(()=>{
     headColor(colors[colorVal],colorVal)
 })
 
+$('#mouthColor').change(()=>{
+    var colorVal = $('#mouthColor').val()
+    mouthColor(colors[colorVal],colorVal)
+})
+
 $('#eyeColor').change(()=>{
     var colorVal = $('#eyeColor').val()
     eyeColor(colors[colorVal],colorVal)
 })
 
 $('#flowerColor').change(()=>{
-  var colorVal = $('#flowerColor').val()
-  flowerColor(colors[colorVal],colorVal)
+    var colorVal = $('#flowerColor').val()
+    flowerColor(colors[colorVal],colorVal)
 })
 
 $('#dressColor').change(()=>{
-  var colorVal = $('#dressColor').val()
-  dressColor(colors[colorVal],colorVal)
+    var colorVal = $('#dressColor').val()
+    dressColor(colors[colorVal],colorVal)
 })
 
 // Changing Shapes
-
 $('#eyeShape').change(()=>{
   var shape = parseInt( $('#eyeShape').val() )
   eyeVariation(shape)
 })
-$('#braceletShape').change(()=>{
-  var shape = parseInt( $('#braceletShape').val() )
-  decorationVariation(shape)
+$('#fashionShape').change(()=>{
+  var shape = parseInt( $('#fashionShape').val() )
+  fashionVariation(shape)
 })
-
-$('#dressShape').change(()=>{
-  var shape = parseInt( $('#dressShape').val() )
-  dressVariation(shape)
-})
-
 // Animations
-
 $('#animation').change(()=>{
   var animationVal = parseInt( $('#animation').val() ) // parsInt to convert string to a integer
   animationVariation(animationVal)
