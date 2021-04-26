de1142fc6a6cf8080302e30e2f0452b4890d4175
anditech <71329092+anditech@users.noreply.github.com>
2021-04-25 20:13:45 -0400

    Update after grade 90/100
    Modification:
    html :
    changed
    <tittle>
    identation
    <labels>
    
    css
    animation
    
    js lady Factory
    comments update
    cleaned code

--- client/assets/js/ladyFactory.js
+++ client/assets/js/ladyFactory.js
@@ -19,58 +19,53 @@ function headColor(color,code) {
     $('#headcode').html('code: ' + code); //This updates text of the badge next to the slider
     $('#dnabody').html(code); // This updates the body color part of the DNA that is displayed below the lady
 }
-
 function mouthColor(color,code) {
     $('.mouth').css('background', '#' + color);  //This changes the color of the mouth
     $('#mouthCode').html('code: ' + code);  //This updates text of the badge next to the slider
     $('#dnamouth').html(code);  // This updates the mouth color part of the DNA that is displayed below the lady
 }
-
 function eyeColor(color,code) {
-    $('.pupils').css('background', '#' + color);  // This changes thre color of the eyes
+    $('.pupils').css('background', '#' + color);  // This changes the color of the eyes
     $('#eyeCode').html('code: ' + code);  // This updates text of the badge next to slider
-    $('#dnaeyes').html(code);  // This updates the body color part of the DNA that is displayed below the lady
+    $('#dnaeyes').html(code);  // This updates the eye color part of the DNA that is displayed below the lady
 }
 function dressColor(color, code) {
-    $('.dress').css('background', '#' + color);   // This changes thre color of the dress
+    $('.dress').css('background', '#' + color);   // This changes the color of the dress
     $('#dressCode').html('code: ' + code);   // This updates text of the badge next to slider
-    $('#dnadress').html(code);   // This updates the body color part of the DNA that is displayed below the lady
+    $('#dnadress').html(code);   // This updates the dress color part of the DNA that is displayed below the lady
 }
 function flowerColor(color,code) {
-    $('.missTake').css('--flower-side-color', '#' + color);   // This changes thre color of the center petals
+    $('.missTake').css('--flower-side-color', '#' + color);   // This changes the color of the center petals
     $('#flowerCode').html('code: ' + code);   // This updates text of the badge next to slider
     $('#dnaflower').html(code);   // This updates the flower part of the DNA that is displayed below the lady
 }
 function flowerColor2(color, code) {
-    $('.missTake').css('--flower-mid-color', '#' + color);   // This changes thre color of the side petals
+    $('.missTake').css('--flower-mid-color', '#' + color);   // This changes the color of the side petals
     $('#flowerCode2').html('code: ' + code);   // This updates text of the badge next to slider
     $('#dnaFlowerColor2').html(code);   // This updates the flower part of the DNA that is displayed below the lady
 }
 
-
-// Variation functions for range-bars 
-// Eyes variation
 function eyeVariation(num) {
 
     $('#dnaEyeShape').html(num)
     switch (num) {
         case 1:
             normalEyes();
-            $('#eyeName').html('Attentive'); // Set badge to "Attentive"
+            $('#eyeName').html('Attentive'); // Sets badge to shape: "Attentive"
             break;
         case 2:
             normalEyes();  // Reset
-            $('#eyeName').html('Relaxed');  // Set badge to "Relaxed"
+            $('#eyeName').html('Relaxed');  // Sets badge to shape: "Relaxed"
             eyesType1();
             break;   
         case 3:
             normalEyes(); // Reset
-            $('#eyeName').html('Bitchy');  // Set badge to "Bitchy"
+            $('#eyeName').html('Bitchy');  // Sets badge to shape: "Bitchy"
             eyesType2(); 
             break;   
         case 4:
             normalEyes();  // Reset
-            $('#eyeName').html('Duuuh');  // Set badge to "Duuh"
+            $('#eyeName').html('Duuuh');  // Sets badge to shape: "Duuh"
             eyesType3();
             break; 
     }
@@ -86,64 +81,41 @@ function fashionVariation(num) {
             $('#fashionName').html('Free | Wide'); // Set badge to Free Wide          
             break; 
         case 2:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Neck | Medium'); // Set badge to Neck Medium     
             dressType1();     
             decorationType1();
             break;   
         case 3:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Shoulder | Fine'); // Set badge to Shoulder Fine      
             dressType2();       
             decorationType2();
             break; 
-        case 3:
-            normalDress();
-            normalDecoration();  // Reset
-            $('#fashionName').html('Shoulder | Fine'); // Set badge to Shoulder Fine      
-            dressType2();       
-            decorationType2();
-            break;
         case 4:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Free | Medium'); // Set badge to Free Medium      
             normalDress();      
             decorationType1();
             break
         case 5:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Neck | Fine'); // Set badge to Neck Fine      
             dressType1();        
             decorationType2();
             break;   
         case 6:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Shoulder | Wide'); // Set badge to Shoulder Wide     
             dressType2();       
             normalDecoration(); 
             break;
         case 7:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Free | Fine'); // Set badge to Free Fine      
             normalDress();        
             decorationType2();
             break; 
         case 8:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Neck| Wide'); // Set badge to Neck Wide      
             dressType1();        
             normalDecoration();
             break;
         case 9:
-            normalDress();
-            normalDecoration();  // Reset
             $('#fashionName').html('Shoulder | Medium'); // Set badge to Shoulder Medium      
             dressType2();        
             decorationType1();
@@ -183,27 +155,27 @@ function animationType1() {
 }
 function animationType2() {
     resetAnimation();     // Resets animation 
-    $(".d1").addClass("shinyDiamond");    // adding diamond animation   
+    $(".d1").addClass("shinyDiamond");    // Adds diamond animation   
 }
 function animationType3() {
     resetAnimation();
-    $(".theHair").addClass("shinyHair");   // adding hair animation
+    $(".theHair").addClass("shinyHair");   // Adds hair animation
 }
 function animationType4() {
     resetAnimation();   // Resets animation 
-    $("#theArms").addClass("dancingQueen");   // adding dancing queen animation   
+    $("#theArms").addClass("dancingQueen");   // Adds dancing queen animation   
 }
 function animationType5() {
     $("#theHead").addClass("movingHead");
     $(".d1").addClass("shinyDiamond");
     $(".theHair").addClass("shinyHair");
-    $("#theArms").addClass("dancingQueen");   // adding hair | diamond  | dancing queen animation   
+    $("#theArms").addClass("dancingQueen");   // Adds ALL animations  
 }
 function resetAnimation() {
     $("#theHead").removeClass("movingHead");
     $(".d1").removeClass("shinyDiamond");
     $(".theHair").removeClass("shinyHair");
-    $("#theArms").removeClass("dancingQueen");
+    $("#theArms").removeClass("dancingQueen");  // Resets animations 
 }
 // Functions Eye Variations
 function normalEyes() {
@@ -220,21 +192,21 @@ function eyesType3() {
 }
 // Functions Bracelet variations 
 function normalDecoration() {
-    $('.arms').find('span').css({"transform": "rotate(0deg)", "height": "30px", "width": "200px", "top": "120px", "border-radius": "10% 10% 10% 10%" });
+    $('.arms').find('span').css({"height": "30px", "width": "200px", "top": "120px", "border-radius": "10% 10% 10% 10%" });
 }
 function decorationType1() {
-    $('.arms').find('span').css({ "transform": "rotate(0deg)", "height": "38px", "width": "200px", "top": "135px", "border-radius": "40% 40% 40% 40%" });
+    $('.arms').find('span').css({"height": "38px", "width": "200px", "top": "135px", "border-radius": "40% 40% 40% 40%" });
 }
 function decorationType2() {
-    $('.arms').find('span').css({ "transform": "rotate(0deg)", "height": "18px", "width": "200px", "top": "140px", "border-radius": "60% 60% 60% 60%" });
+    $('.arms').find('span').css({"height": "18px", "width": "200px", "top": "140px", "border-radius": "60% 60% 60% 60%" });
 }
 // Functions Dress variations
 function normalDress() {
-    $('.dress').css({"border": "none", "top": "20px", "left": "28px","border-radius": "50% 50% 90% 90%"});
+    $('.dress').css({"top": "20px", "border-radius": "50% 50% 90% 90%"});
 }
 function dressType1() {
-    $('.dress').css({"border": "none", "top": "2px", "left": "28px","border-radius": "120% 120% 90% 90%"});
+    $('.dress').css({"top": "2px", "border-radius": "120% 120% 90% 90%"});
 }
 function dressType2() {
-    $('.dress').css({"border": "none", "top": "2px", "left": "28px","border-radius": "185% 115% 90% 0%"});
+    $('.dress').css({"top": "2px", "border-radius": "185% 115% 90% 0%"});
 }
